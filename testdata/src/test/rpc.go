5fa4aa1e184b3a82b818259cd42d15b1f686ea0f
raeperd <raeperd117@gmail.com>
2024-09-14 07:39:54 +0900

    test: Add reference in testdata

--- testdata/src/test/rpc.go
+++ testdata/src/test/rpc.go
@@ -18,3 +18,16 @@ func (rpc *RPC) compute() {
 func (RPC) version() int {
 	return 1 // never going to need to change this
 }
+
+// Following main function cause data race error
+// reference: https://dave.cheney.net/2015/11/18/wednesday-pop-quiz-spot-the-race
+// func main() {
+// 	rpc := &RPC{done: make(chan struct{})}
+//
+// 	go rpc.compute()         // kick off computation in the background
+// 	version := rpc.version() // grab some other information while we're waiting
+// 	<-rpc.done               // wait for computation to finish
+// 	result := rpc.result
+//
+// 	fmt.Printf("RPC computation complete, result: %d, version: %d\n", result, version)
+// }
