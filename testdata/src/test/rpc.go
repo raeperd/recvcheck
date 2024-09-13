3f8df700c194c1a7a5534d8edd3b0e46026ee511
raeperd <raeperd117@gmail.com>
2024-09-14 07:28:32 +0900

    test: Fix testcase to report on struct

--- testdata/src/test/rpc.go
+++ testdata/src/test/rpc.go
@@ -4,7 +4,7 @@ import (
 	"time"
 )
 
-type RPC struct {
+type RPC struct { // want `the methods of "RPC" use pointer receiver and non pointer receiver.`
 	result int
 	done   chan struct{}
 }
@@ -15,6 +15,6 @@ func (rpc *RPC) compute() {
 	close(rpc.done)
 }
 
-func (RPC) version() int { // want "receiver type should be a pointer"
+func (RPC) version() int {
 	return 1 // never going to need to change this
 }
