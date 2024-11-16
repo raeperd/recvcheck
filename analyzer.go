a29056eabf0a02a3021d2caeabac305934eca5a9
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-11-16 16:23:47 +0100

    review: remove closures

--- analyzer.go
+++ analyzer.go
@@ -24,10 +24,12 @@ func NewAnalyzer(s Setting) *analysis.Analyzer {
 		excludeMethods = map[string]struct{}{}
 	}
 
+	a := &analyzer{excludeMethods: excludeMethods}
+
 	return &analysis.Analyzer{
 		Name:     "recvcheck",
 		Doc:      "checks for receiver type consistency",
-		Run:      run(newFilter(excludeMethods)),
+		Run:      a.run,
 		Requires: []*analysis.Analyzer{inspect.Analyzer},
 	}
 }
@@ -45,70 +47,70 @@ type Setting struct {
 	DisableBuiltin bool
 }
 
-func run(filter func(*ast.FuncDecl) bool) func(*analysis.Pass) (any, error) {
-	return func(pass *analysis.Pass) (any, error) {
-		inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+type analyzer struct {
+	excludeMethods map[string]struct{}
+}
 
-		structs := map[string]*structType{}
-		inspector.Preorder([]ast.Node{(*ast.FuncDecl)(nil)}, func(n ast.Node) {
-			funcDecl, ok := n.(*ast.FuncDecl)
-			if !ok || funcDecl.Recv == nil || len(funcDecl.Recv.List) != 1 {
-				return
-			}
+func (r *analyzer) run(pass *analysis.Pass) (any, error) {
+	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
-			if filter(funcDecl) {
-				return
-			}
-
-			var recv *ast.Ident
-			var isStar bool
-			switch recvType := funcDecl.Recv.List[0].Type.(type) {
-			case *ast.StarExpr:
-				isStar = true
-				if recv, ok = recvType.X.(*ast.Ident); !ok {
-					return
-				}
-			case *ast.Ident:
-				recv = recvType
-			default:
-				return
-			}
-
-			st, ok := structs[recv.Name]
-			if !ok {
-				structs[recv.Name] = &structType{}
-				st = structs[recv.Name]
-			}
-
-			if isStar {
-				st.starUsed = true
-			} else {
-				st.typeUsed = true
-			}
-		})
-
-		for recv, st := range structs {
-			if st.starUsed && st.typeUsed {
-				pass.Reportf(pass.Pkg.Scope().Lookup(recv).Pos(), "the methods of %q use pointer receiver and non-pointer receiver.", recv)
-			}
+	structs := map[string]*structType{}
+	inspector.Preorder([]ast.Node{(*ast.FuncDecl)(nil)}, func(n ast.Node) {
+		funcDecl, ok := n.(*ast.FuncDecl)
+		if !ok || funcDecl.Recv == nil || len(funcDecl.Recv.List) != 1 {
+			return
 		}
 
-		return nil, nil
+		if r.isExcluded(funcDecl) {
+			return
+		}
+
+		var recv *ast.Ident
+		var isStar bool
+		switch recvType := funcDecl.Recv.List[0].Type.(type) {
+		case *ast.StarExpr:
+			isStar = true
+			if recv, ok = recvType.X.(*ast.Ident); !ok {
+				return
+			}
+		case *ast.Ident:
+			recv = recvType
+		default:
+			return
+		}
+
+		st, ok := structs[recv.Name]
+		if !ok {
+			structs[recv.Name] = &structType{}
+			st = structs[recv.Name]
+		}
+
+		if isStar {
+			st.starUsed = true
+		} else {
+			st.typeUsed = true
+		}
+	})
+
+	for recv, st := range structs {
+		if st.starUsed && st.typeUsed {
+			pass.Reportf(pass.Pkg.Scope().Lookup(recv).Pos(), "the methods of %q use pointer receiver and non-pointer receiver.", recv)
+		}
 	}
+
+	return nil, nil
+}
+
+func (r *analyzer) isExcluded(f *ast.FuncDecl) bool {
+	if f.Name == nil || f.Name.Name == "" {
+		return true
+	}
+
+	_, found := r.excludeMethods[f.Name.Name]
+	return found
 }
 
 type structType struct {
 	starUsed bool
 	typeUsed bool
-}
-
-func newFilter(excludes map[string]struct{}) func(*ast.FuncDecl) bool {
-	return func(f *ast.FuncDecl) bool {
-		if f.Name == nil || f.Name.Name == "" {
-			return true
-		}
-
-		_, found := excludes[f.Name.Name]
-		return found
-	}
 }
